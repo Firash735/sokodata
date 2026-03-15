08343cdea2c3ee236788f5641643633216cf540f
Sharif <sharif@Sharifs-MacBook-Air-173.local>
2026-03-15 22:38:34 +0300

    fix: remove exposed keys from index.html

---
+++ frontend/js/env.js
@@ -0,0 +1,4 @@
+window.__SOKO__ = {
+  url:  'https://mbwvwiqktikzflkfqiph.supabase.co',
+  anon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1id3Z3aXFrdGlremZsa2ZxaXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0Nzk1NTQsImV4cCI6MjA4OTA1NTU1NH0.dJQqJUCwheblZyqh_83aALJSWIq6WiY5QpBEYT6HUeo',
+}
